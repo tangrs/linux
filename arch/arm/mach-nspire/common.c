05eb33dcadb54d84c70e2eed163784b566e2d162
Fabian Vogt <fabian@ritter-vogt.de>
2013-01-18 18:09:46 +0100

    Added support for the touchpad
    Signed-off-by: Fabian Vogt <fabian@ritter-vogt.de>

--- arch/arm/mach-nspire/common.c
+++ arch/arm/mach-nspire/common.c
@@ -58,7 +58,8 @@ static struct clk apb_clk = {
 
 #ifdef CONFIG_MACH_NSPIRECX
 static struct clk i2c_clk = {
-	.rate	= 100000,
+	/* Doesn't matter, we set it manually */
+	.rate	= 250000,
 };
 #endif
 
