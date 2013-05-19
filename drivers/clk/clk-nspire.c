af8fd493adcae0fe37c3fd25fe506f13d454f8da
Daniel Tang <dt.tangr@gmail.com>
2013-05-19 21:06:27 +1000

    Change clk-nspire to use compatible property to determine IO type

--- drivers/clk/clk-nspire.c
+++ drivers/clk/clk-nspire.c
@@ -35,72 +35,54 @@
 #define CX_UNKNOWN_SHIFT	21
 #define CX_UNKNOWN_MASK		0x03
 
-#define EXTRACT(var, prop) (((var)>>prop##_SHIFT) & prop##_MASK)
-
 struct nspire_clk_info {
 	u32 base_clock;
 	u16 base_cpu_ratio;
 	u16 base_ahb_ratio;
 };
 
-static int nspire_clk_read(struct device_node *node,
-		struct nspire_clk_info *clk)
+
+#define EXTRACT(var, prop) (((var)>>prop##_SHIFT) & prop##_MASK)
+static void nspire_clkinfo_cx(u32 val, struct nspire_clk_info *clk)
 {
-	u32 val;
-	int ret;
-	void __iomem *io;
-	const char *type = NULL;
+	if (EXTRACT(val, FIXED_BASE))
+		clk->base_clock = 48 * MHZ;
+	else
+		clk->base_clock = 6 * EXTRACT(val, CX_BASE) * MHZ;
 
-	ret = of_property_read_string(node, "io-type", &type);
-	if (ret)
-		return ret;
-
-	io = of_iomap(node, 0);
-	if (!io)
-		return -ENOMEM;
-	val = readl(io);
-	iounmap(io);
-
-	if (!strcmp(type, "cx")) {
-		if (EXTRACT(val, FIXED_BASE)) {
-			clk->base_clock = 48 * MHZ;
-		} else {
-			clk->base_clock = 6 * EXTRACT(val, CX_BASE) * MHZ;
-		}
-
-		clk->base_cpu_ratio = EXTRACT(val, BASE_CPU) *
-					EXTRACT(val, CX_UNKNOWN);
-		clk->base_ahb_ratio = clk->base_cpu_ratio *
-					(EXTRACT(val, CPU_AHB) + 1);
-	} else if (!strcmp(type, "classic")) {
-		if (EXTRACT(val, FIXED_BASE)) {
-			clk->base_clock = 27 * MHZ;
-		} else {
-			clk->base_clock = (300 -
-					6 * EXTRACT(val, CLASSIC_BASE)) * MHZ;
-		}
-
-		clk->base_cpu_ratio = EXTRACT(val, BASE_CPU) * 2;
-		clk->base_ahb_ratio = clk->base_cpu_ratio *
-					(EXTRACT(val, CPU_AHB) + 1);
-	} else {
-		return -EINVAL;
-	}
-
-	return 0;
+	clk->base_cpu_ratio = EXTRACT(val, BASE_CPU) * EXTRACT(val, CX_UNKNOWN);
+	clk->base_ahb_ratio = clk->base_cpu_ratio * (EXTRACT(val, CPU_AHB) + 1);
 }
 
-static void __init nspire_ahbdiv_setup(struct device_node *node)
+static void nspire_clkinfo_classic(u32 val, struct nspire_clk_info *clk)
 {
-	int ret;
+	if (EXTRACT(val, FIXED_BASE))
+		clk->base_clock = 27 * MHZ;
+	else
+		clk->base_clock = (300 - 6 * EXTRACT(val, CLASSIC_BASE)) * MHZ;
+
+	clk->base_cpu_ratio = EXTRACT(val, BASE_CPU) * 2;
+	clk->base_ahb_ratio = clk->base_cpu_ratio * (EXTRACT(val, CPU_AHB) + 1);
+}
+#undef EXTRACT
+
+static void __init nspire_ahbdiv_setup(struct device_node *node,
+		void (*get_clkinfo)(u32, struct nspire_clk_info *))
+{
+	u32 val;
+	void __iomem *io;
 	struct clk *clk;
 	const char *clk_name = node->name;
 	const char *parent_name;
 	struct nspire_clk_info info;
 
-	ret = nspire_clk_read(node, &info);
-	if (WARN_ON(ret))
+	io = of_iomap(node, 0);
+	if (!io)
 		return;
+	val = readl(io);
+	iounmap(io);
+
+	get_clkinfo(val, &info);
 
 	of_property_read_string(node, "clock-output-names", &clk_name);
 	parent_name = of_clk_get_parent_name(node, 0);
@@ -111,16 +93,37 @@ static void __init nspire_ahbdiv_setup(struct device_node *node)
 		of_clk_add_provider(node, of_clk_src_simple_get, clk);
 }
 
-static void __init nspire_clk_setup(struct device_node *node)
+static void __init nspire_ahbdiv_setup_cx(struct device_node *node)
 {
-	int ret;
+	nspire_ahbdiv_setup(node, nspire_clkinfo_cx);
+}
+
+static void __init nspire_ahbdiv_setup_classic(struct device_node *node)
+{
+	nspire_ahbdiv_setup(node, nspire_clkinfo_classic);
+}
+
+CLK_OF_DECLARE(nspire_ahbdiv_cx, "nspire-ahb-divider-cx",
+		nspire_ahbdiv_setup_cx);
+CLK_OF_DECLARE(nspire_ahbdiv_classic, "nspire-ahb-divider-classic",
+		nspire_ahbdiv_setup_classic);
+
+static void __init nspire_clk_setup(struct device_node *node,
+		void (*get_clkinfo)(u32, struct nspire_clk_info *))
+{
+	u32 val;
+	void __iomem *io;
 	struct clk *clk;
 	const char *clk_name = node->name;
 	struct nspire_clk_info info;
 
-	ret = nspire_clk_read(node, &info);
-	if (WARN_ON(ret))
+	io = of_iomap(node, 0);
+	if (!io)
 		return;
+	val = readl(io);
+	iounmap(io);
+
+	get_clkinfo(val, &info);
 
 	of_property_read_string(node, "clock-output-names", &clk_name);
 
@@ -137,5 +140,16 @@ static void __init nspire_clk_setup(struct device_node *node)
 		info.base_clock / info.base_ahb_ratio / MHZ);
 }
 
-CLK_OF_DECLARE(nspire_clk, "nspire-clock", nspire_clk_setup);
-CLK_OF_DECLARE(nspire_ahbdiv, "nspire-ahb-divider", nspire_ahbdiv_setup);
+static void __init nspire_clk_setup_cx(struct device_node *node)
+{
+	nspire_clk_setup(node, nspire_clkinfo_cx);
+}
+
+static void __init nspire_clk_setup_classic(struct device_node *node)
+{
+	nspire_clk_setup(node, nspire_clkinfo_classic);
+}
+
+CLK_OF_DECLARE(nspire_clk_cx, "nspire-clock-cx", nspire_clk_setup_cx);
+CLK_OF_DECLARE(nspire_clk_classic, "nspire-clock-classic",
+		nspire_clk_setup_classic);
